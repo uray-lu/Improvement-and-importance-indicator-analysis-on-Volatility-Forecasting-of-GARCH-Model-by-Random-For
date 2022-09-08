394b4db8693c523c86d098616b32fb9013cd0a97
Ray <george02270227@gmail.com>
2022-09-08 20:18:42 +0800

    Review and Modify

--- src/descriptive_stat.py
+++ src/descriptive_stat.py
@@ -17,57 +17,75 @@ from colorama import init, Fore
 import logging
 from datetime import datetime
 from pathlib import Path
+import warnings
+
+##review
+
 
 
 class descriptive_stat:
     
     def __init__(self, directory):
         
+        
+        init(autoreset = True)
+        logging.getLogger().setLevel(logging.INFO)
+        warnings.simplefilter('ignore')
+        
         self.directory = directory
-    
-    def check_data(self):
         
-        raw = pd.read_csv(self.directory)
+        csv_files = glob.glob(os.path.join(self.directory, '*.csv'))
+        layout = []
         
-        if len(raw.columns) == 2:
+        for f in csv_files:
             
-            input_data = raw.iloc[:, -1]
+            raw = pd.read_csv(f)
             
-            df = pd.DataFrame(input_data)
-            df.columns = [str(Path(self.directory).stem)]
-          
-        else:
-            
-            if 'Close' in raw.columns:
+            if len(raw.columns) == 2:
                 
-                input_data = raw['Close']
-            
-            elif 'close' in raw.columns:
+                input_data = raw.iloc[:, -1]
                 
-                input_data = raw['close']
-            
-            elif 'Price' in raw.columns:
+                df = pd.DataFrame(input_data)
+                df.columns = [str(Path(f).stem)]
+              
+            else:
                 
-                input_data = raw['Price']
+                if 'Close' in raw.columns:
+                    
+                    input_data = raw['Close']
                 
-            elif ' Close' in raw.columns:
+                elif 'close' in raw.columns:
+                    
+                    input_data = raw['close']
                 
-                input_data = raw[' Close']
+                elif 'Price' in raw.columns:
+                    
+                    input_data = raw['Price']
+                    
+                elif ' Close' in raw.columns:
+                    
+                    input_data = raw[' Close']
+                
+                df = pd.DataFrame(input_data, )
+                df.columns = [str(Path(f).stem)]
+                
             
-            df = pd.DataFrame(input_data, )
-            df.columns = [str(Path(self.directory).stem)]
             
+            try:
+               
+                df = self.to_int(df[str(Path(f).stem)])  
+            
+            except:
+                
+                pass
+            
+            #df = pd.DataFrame(df.T)
+            
+            layout.append(df)
+            
+        self.layout = layout
         
-        try:
-           
-            df = self.to_int(df[str(Path(self.directory).stem)])  
         
-        except:
-            
-            pass
-            
-
-        return df
             
 
     def stat(self, x) : 
@@ -76,22 +94,58 @@ class descriptive_stat:
                      '%.2f'%x.std(), '%.2f'%x.skew(), '%.2f'%x.kurt()],
                      index=['Observation','Min','Mean','Max','Var','Std','Skwness','Kurtosis'])
 
-    def output(self):
+    def mkoutput(self):
         
-            
         
-        try:
-            stats_data = self.check_data().apply(self.stat)
+        try :
+            des_output = pd.DataFrame()    
+        
+            for i in range(len(self.layout)):
             
-            logging.info(f"{(datetime.now().strftime('%Y-%m-%d %H:%M:%S')):<10}{' ' + str(Path(self.directory).stem): ^10}{' Descripitive Statistic': ^10}{'·'*20: ^10}{Fore.GREEN}{'Pass'}")
+                stats_data = self.layout[i].apply(self.stat)    
+                stats_data = pd.DataFrame(stats_data)
     
+                des_output = des_output.append(stats_data.T)
+            
+        
+            des_output = des_output.sort_values(['Observation'], ascending= False)
+            des_output = des_output.T
+        
+        
+            print(des_output)
+            
+            logging.info(f"{(datetime.now().strftime('%Y-%m-%d %H:%M:%S')):<10}{' ' + str(Path(self.directory).stem): ^10}{'Descripitive Statistic': ^10}{'·'*20: ^10}{Fore.GREEN}{'Pass'}")
+            
+       
         except:
         
-            logging.info(f"{(datetime.now().strftime('%Y-%m-%d %H:%M:%S')):<10}{' ' +str(Path(self.directory).stem): ^10}{' Descripitive Statistic': ^10}{'·'*20: ^10}{Fore.RED}{'Error'}")    
+            logging.info(f"{(datetime.now().strftime('%Y-%m-%d %H:%M:%S')):<10}{' ' +str(Path(self.directory).stem): ^10}{'Descripitive Statistic': ^10}{'·'*20: ^10}{Fore.RED}{'Error'}")    
     
         
+        root_path = os.getcwd()[:os.getcwd().find('/bitcoin-volatility-forecast-improvement-through-random-forest-algorithm')+len('bitcoin-volatility-forecast-improvement-through-random-forest-algorithm/')]
+
+
+        try:
+            
+            if os.path.isdir(root_path+'/report/descriptive_statistic') == True:
+                    
+                pass
+            
+            else:   
+                os.mkdir(root_path+'/report')
+                os.mkdir(root_path+'/report/descriptive_statistic')
+            
+            des_output.to_csv(root_path+'/report/descriptive_statistic/'+ str(Path(self.directory).stem) + '.csv')
+            logging.info(f"{(datetime.now().strftime('%Y-%m-%d %H:%M:%S')):<10}{' ' + str(Path(subdir).stem): ^10}{'Descriptive statistic stored': ^10}{'·'*20: ^10}{Fore.GREEN}{'Pass'}")    
+
+        except:
+            
+            logging.info(f"{(datetime.now().strftime('%Y-%m-%d %H:%M:%S')):<10}{' ' + str(Path(subdir).stem): ^10}{'Descriptive statistic store': ^10}{'·'*20: ^10}{Fore.RED}{'Error'}")    
+             
+    
+    
+            
         
-        return stats_data
     
     def to_int(self, x):
         
@@ -104,52 +158,18 @@ class descriptive_stat:
         
         
     
-init(autoreset = True)
-logging.getLogger().setLevel(logging.INFO)
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-subdir", "--subdirectory", help="enter subdirectory name")
-params = parser.parse_args()
-subdir = params.subdirectory
-subdir = str(subdir)
-
-csv_files = glob.glob(os.path.join(subdir, '*.csv'))
 
 
-layout = pd.DataFrame()
-for f in csv_files:
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-subdir", "--subdirectory", help="enter subdirectory name")
+    params = parser.parse_args()
+    subdir = params.subdirectory
+    subdir = str(subdir)
     
-    
-    data = descriptive_stat(f)
-    data = data.output().T
-    
-    layout = layout.append(data)
-
-
-layout = layout.sort_values('Observation', ascending = False)
-layout = layout.T
-print(layout)
-
-
-root_path = os.getcwd()[:os.getcwd().find('/bitcoin-volatility-forecast-improvement-through-random-forest-algorithm')+len('bitcoin-volatility-forecast-improvement-through-random-forest-algorithm/')]
-
-
-try:
-    
-    if os.path.isdir(root_path+'/report/descriptive_statistic') == True:
-            
-        pass
-    
-    else:   
-        os.mkdir(root_path+'/report')
-        os.mkdir(root_path+'/report/descriptive_statistic')
-    
-    layout.to_csv(root_path+'/report/descriptive_statistic/'+ str(Path(subdir).stem) + '.csv')
-    logging.info(f"{(datetime.now().strftime('%Y-%m-%d %H:%M:%S')):<10}{' ' + str(Path(subdir).stem): ^10}{'descriptive statistic stored': ^10}{'·'*20: ^10}{Fore.GREEN}{'Pass'}")    
-
-except:
-    
-    logging.info(f"{(datetime.now().strftime('%Y-%m-%d %H:%M:%S')):<10}{' ' + str(Path(subdir).stem): ^10}{'descriptive statistic store': ^10}{'·'*20: ^10}{Fore.RED}{'Error'}")    
+    data = descriptive_stat(subdir)
+    data.mkoutput()
 
 
 
